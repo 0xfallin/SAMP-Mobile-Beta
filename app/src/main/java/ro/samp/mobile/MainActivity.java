a912e5feb7404d0f7476bc165c8887ee251823b6
0xfallin <johncedd15@gmail.com>
2025-12-05 11:27:58 +0800

    test

--- app/src/main/java/ro/samp/mobile/MainActivity.java
+++ app/src/main/java/ro/samp/mobile/MainActivity.java
@@ -1,34 +1,49 @@
 package ro.samp.mobile;
 
+import android.app.Activity;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
-import androidx.appcompat.app.AppCompatActivity;
+import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
-    private SampLauncher launcher;
+    static {
+        System.loadLibrary("GTASA");
+        System.loadLibrary("samp");
+    }
+
+    // Start SAMP using settings.ini
+    public native void startSAMP();
+
+    // Save settings to settings.ini
+    public native void saveSettings(String host, int port, String nickname);
+
+    private EditText edtHost, edtPort, edtNick;
+    private Button btnSave, btnPlay;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        launcher = new SampLauncher();
+        edtHost = findViewById(R.id.edtHost);
+        edtPort = findViewById(R.id.edtPort);
+        edtNick = findViewById(R.id.edtNick);
+        btnSave = findViewById(R.id.btnSave);
+        btnPlay = findViewById(R.id.btnPlay);
 
-        Button btnPlay = findViewById(R.id.btnPlay);
-        btnPlay.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Run SAMP in a separate thread
-                new Thread(new Runnable() {
-                    @Override
-                    public void run() {
-                        launcher.initSAMP(getFilesDir().getAbsolutePath());
-                        launcher.startGameLoop();
-                    }
-                }).start();
-            }
+        // Save settings to settings.ini
+        btnSave.setOnClickListener(v -> {
+            String host = edtHost.getText().toString();
+            int port = Integer.parseInt(edtPort.getText().toString());
+            String nick = edtNick.getText().toString();
+
+            saveSettings(host, port, nick);
+        });
+
+        // Start SAMP using saved settings
+        btnPlay.setOnClickListener(v -> {
+            new Thread(() -> startSAMP()).start();
         });
     }
 }
